20f0ec8460ca7db441d9d5b764cbe2e975b3d4a9
StandardGiraffe <dannyfekete@gmail.com>
2018-08-30 21:02:41 +0000

    Imported completed roll-die exercise from Nick.

---
+++ loaded-die.js
@@ -0,0 +1,22 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var roll = -1;
+
+
+  return function() {
+    /* your code here */
+    roll += 1;
+
+    if(roll >= list.length){
+      roll = 0
+    }
+    return list[roll];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
